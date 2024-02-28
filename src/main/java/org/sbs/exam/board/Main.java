27134c8325e05c9f32661731a45bacb909038ddd
jcons/7301 <jcons880@gmail.com>
2024-02-28 15:59:58 +0900

    toString override

--- src/main/java/org/sbs/exam/board/Main.java
+++ src/main/java/org/sbs/exam/board/Main.java
@@ -1,11 +1,64 @@
 package org.sbs.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int articleLastId = 0;
 
     System.out.println("==== 게시판 v 0.1 ===");
     System.out.println("=== 게시판 시작 ===");
-    System.out.println("명렫)");
+
+    while (true){
+      System.out.printf("명렫)");
+      String cmd = sc.nextLine();
+
+
+      if(cmd.equals("/usr/article/write")){
+        System.out.println("=== 게시물 등록 ===");
+        System.out.printf("제목: ");
+        String title = sc.nextLine();
+        System.out.printf("내용: ");
+        String body = sc.nextLine();
+
+        int id = articleLastId + 1;
+
+        Article article = new Article();
+        article.id = id;
+        article.title = title;
+        article.body = body;
+
+        System.out.println("생성된 게시물 객체" + " " + article);
+        System.out.printf("%d번 게시물이 등록 되었습니다\n", article.id);
+        articleLastId++;
+
+
+      }
+      else if(cmd.equals("exit")){
+        System.out.println("프로그램을 종료합니다");
+        break;
+      }
+
+      else{
+        System.out.println("명령을 다시 입력해 주세요");
+      }
+    }
+
     System.out.println("=== 게시판 실행 끝");
+
+    sc.close();
+  }
+}
+
+
+class Article{
+  int id;
+  String title;
+  String body;
+
+  @Override
+  public String toString(){
+    return String.format("{id : %d, title:\"%s\", body:\"%s\"}", id, title, body);
   }
 }
