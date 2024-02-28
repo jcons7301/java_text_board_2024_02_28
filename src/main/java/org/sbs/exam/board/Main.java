6c2747a71f6768313112f5e19721bb274a5beae7
jcons/7301 <jcons880@gmail.com>
2024-02-28 17:30:56 +0900

    게시물 list출력

--- src/main/java/org/sbs/exam/board/Main.java
+++ src/main/java/org/sbs/exam/board/Main.java
@@ -1,5 +1,7 @@
 package org.sbs.exam.board;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +9,12 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
     Article lastArticle = null;
+    List<Article> articles = new ArrayList<>();
+
+    //테스트 게시물 3개 등록
+    articles.add(new Article(1, "제목1", "내용1"));
+    articles.add(new Article(2, "제목2", "내용2"));
+    articles.add(new Article(3, "제목3", "내용3"));
 
     System.out.println("==== 게시판 v 0.1 ===");
     System.out.println("=== 게시판 시작 ===");
@@ -34,6 +42,17 @@ public class Main {
 
 
       }
+      else if(cmd.equals("/usr/article/list")){
+        System.out.println("=== 게시물 리스트 ===");
+        System.out.println("--------------------");
+        System.out.println("번호 / 제목");
+
+        for(Article article : articles){
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+        System.out.println("====================");
+      }
+
       else if(cmd.equals("/usr/article/detail")){
         if(lastArticle == null){
           System.out.println("게시물이 존해하지 않습니다");
